d0fc7394c91ab976e4c7748e2722d17a451f2b29
Rod Vagg <rod@vagg.org>
2025-06-04 14:55:48 +1000

    chore: reorg auth components into pdp subpackage, remove from main interface

--- src/pdp/index.ts
+++ src/pdp/index.ts
@@ -1,3 +1,4 @@
 // Export PDP services
 export { PDPUploadService } from './pdp-upload-service.js'
 export { PDPDownloadService } from './pdp-download-service.js'
+export { PDPAuthHelper } from './auth.js'
