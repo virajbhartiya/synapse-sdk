14f8abed5eeb5f1b1090f3ba3ddfc24ac7f4a550
Rod Vagg <rod@vagg.org>
2025-06-05 00:34:42 +1000

    fix(pdp): more complete index

--- src/pdp/index.ts
+++ src/pdp/index.ts
@@ -2,3 +2,5 @@
 export { PDPUploadService } from './pdp-upload-service.js'
 export { PDPDownloadService } from './pdp-download-service.js'
 export { PDPAuthHelper } from './auth.js'
+export { PDPTool } from './tool.js'
+export type { CreateProofSetResponse, ProofSetCreationStatusResponse } from './tool.js'
