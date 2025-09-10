4158abfa2aca84523c40b810acaccd0eadb9de6f
Hugo Dias <hugomrdias@gmail.com>
2025-09-10 12:02:47 +0100

    chore(docs): add GitHub Pages deployment workflow and enable GitHub Pages in Astro config (#210)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -78,6 +78,7 @@ export default defineConfig({
         docsPlugin({
           pagination: true,
           typeDocOptions: {
+            githubPages: true,
             entryPointStrategy: 'resolve',
             entryPoints: [
               '../src/index.ts',
